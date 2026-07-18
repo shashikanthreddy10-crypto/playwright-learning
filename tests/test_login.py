b6ec7ac5d317e546ccb8b0be30fe62be6046f32d
shashi <shashikanthreddy10@gmail.com>
2026-07-18 06:20:02 +0530

    added a print function in login

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,6 +3,6 @@ import pytest
 
 @pytest.mark.parametrize("login",[1,0],indirect=True)
 def test_logintest(login):
-    print("Login page launched")
+    print("Login page launched, loggged in")
     pass
 
